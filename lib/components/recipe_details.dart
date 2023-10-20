bc8b949a756fb1467d438355a3ade3f4ff9fda85
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2023-10-20 16:13:18 +0800

    add tabbar

--- lib/components/recipe_details.dart
+++ lib/components/recipe_details.dart
@@ -11,58 +11,94 @@ class RecipeDetails extends StatelessWidget {
   int i = 1;
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(8),
-              child: AspectRatio(
-                  aspectRatio: 3 / 2,
-                  child: Image.network(
-                    recipe.imageURL,
-                    fit: BoxFit.cover,
-                  )),
-            ),
-            Container(
-              margin: const EdgeInsets.all(16),
-              child: Text(
-                recipe.title,
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.w600,
-                ),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: AspectRatio(
+                aspectRatio: 3 / 2,
+                child: Image.network(
+                  recipe.imageURL,
+                  fit: BoxFit.cover,
+                )),
+          ),
+          Container(
+            margin: const EdgeInsets.all(16),
+            child: Text(
+              recipe.title,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w600,
               ),
             ),
-            Text(recipe.description),
-            Container(
-              margin: const EdgeInsets.only(top: 16, bottom: 16),
-              child: const Text(
-                'Ingredents:',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w600,
-                ),
+          ),
+          Text(recipe.description),
+          const SizedBox(
+            height: 16,
+          ),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(4),
+            child: Container(
+              color: const Color(0xff8D0801),
+              child: const TabBar(
+                tabs: [
+                  Tab(
+                    text: "1",
+                  ),
+                  Tab(
+                    text: "2",
+                  )
+                ],
               ),
             ),
-            ...recipe.ingredients.map((e) => Text('• $e')).toList(),
-            Container(
-              margin: const EdgeInsets.only(top: 16, bottom: 16),
-              child: const Text(
-                'Steps:',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ),
-            ...recipe.steps.map((e) => Text('${i++}. $e')).toList(),
-          ],
-        ),
+          ),
+          Expanded(
+            child: TabDetails(recipe: recipe),
+          )
+        ],
       ),
+    );
+  }
+}
+
+class TabDetails extends StatelessWidget {
+  TabDetails({
+    super.key,
+    required this.recipe,
+  });
+
+  final Recipe recipe;
+
+  @override
+  Widget build(BuildContext context) {
+    return TabBarView(
+      children: <Widget>[
+        ListView.builder(
+          itemBuilder: (_, index) {
+            return Card(
+              child: Container(
+                padding: const EdgeInsets.all(12),
+                child: Text("${index + 1}. ${recipe.ingredients[index]}"),
+              ),
+            );
+          },
+          itemCount: recipe.ingredients.length,
+        ),
+        ListView.builder(
+          itemBuilder: (_, index) {
+            return Card(
+              child: Container(
+                padding: const EdgeInsets.all(12),
+                child: Text("${index + 1}. ${recipe.steps[index]}"),
+              ),
+            );
+          },
+          itemCount: recipe.steps.length,
+        ),
+      ],
     );
   }
 }
